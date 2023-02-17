ea9cc902d982b5191b5b1f2ca9526a7f9e56bf2f
Quin-Darcy <quintondarcy@csus.edu>
2023-02-16 21:41:38 -0800

    added extended members to opt header class

--- optheader.h
+++ optheader.h
@@ -6,6 +6,7 @@
 
 typedef unsigned char BYTE;
 
+
 class OPTHeader
 {
 	public:
@@ -18,7 +19,34 @@ class OPTHeader
 		uint32_t size_of_uninitialized_data;
 		uint32_t address_of_entry_point;
 		uint32_t base_of_code;
+		
 		uint32_t base_of_data;
+		uint32_t image_base;
+		uint64_t image_base_pe_plus;
+		uint32_t section_allignment;
+		uint32_t file_allignment;
+		uint16_t major_operating_system_version;
+		uint16_t minor_operating_system_version;
+		uint16_t major_image_version;
+		uint16_t minor_image_version;
+		uint16_t major_subsystem_version;
+		uint16_t minor_subsystem_version;
+		uint32_t win32_version_value;
+		uint32_t size_of_image;
+		uint32_t size_of_headers;
+		uint32_t checksum;
+		uint16_t subsystem;
+		uint16_t sll_characteristics;
+		uint32_t size_of_stack_reserve;
+		uint64_t size_of_stack_reserve_pe_plus;
+		uint32_t size_of_stack_commit;
+		uint64_t size_of_stack_commit_pe_plus;
+		uint32_t size_of_heap_reserve;
+		uint64_t size_of_heap_reserve_pe_plus;
+		uint32_t size_of_heap_commit;
+		uint64_t size_of_heap_commit_pe_plus;
+		uint32_t loader_flags;
+		uint32_t number_of_rva_and_sizes;
 		
 		OPTHeader(std::vector<BYTE> bytes_from_file, uint32_t offset);
 };
